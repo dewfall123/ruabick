3a71eb9956cb211aaf6254936e218f2b8c029d54
haiweilian <haiweilian@foxmail.com>
2022-08-25 19:11:45 +0800

    fix: tag render rule

--- packages/md-demo-plugins/src/plugin-demo-block.ts
+++ packages/md-demo-plugins/src/plugin-demo-block.ts
@@ -6,36 +6,41 @@ import { getDemoComponent } from './utils';
 import fsExtra from 'fs-extra';
 
 export function demoBlockPlugin(md: MarkdownRenderer) {
-  const defaultRender = md.renderer.rules.html_block;
+  const addRenderRule = (type: string) => {
+    const defaultRender = md.renderer.rules[type];
 
-  md.renderer.rules.html_block = (tokens, idx, options, env, self) => {
-    const token = tokens[idx];
-    const content = token.content.trim();
+    md.renderer.rules[type] = (tokens, idx, options, env, self) => {
+      const token = tokens[idx];
+      const content = token.content.trim();
 
-    if (!content.startsWith(`<${DemoTag} `)) {
-      return defaultRender!(tokens, idx, options, env, self);
-    }
+      if (!content.startsWith(`<${DemoTag} `)) {
+        return defaultRender!(tokens, idx, options, env, self);
+      }
 
-    const props = parseProps(content);
+      const props = parseProps(content);
 
-    if (!props.src) {
-      console.error(`miss src props in ${md.__path} demo.`);
-      return defaultRender!(tokens, idx, options, env, self);
-    }
+      if (!props.src) {
+        console.error(`miss src props in ${md.__path} demo.`);
+        return defaultRender!(tokens, idx, options, env, self);
+      }
 
-    const frontmatter = env.frontmatter;
+      const frontmatter = env.frontmatter;
 
-    const mdDir = dirname(frontmatter.realPath ?? md.__path);
-    const srcPath = resolve(mdDir, props.src);
-    const code = fsExtra.readFileSync(srcPath, 'utf-8');
+      const mdDir = dirname(frontmatter.realPath ?? md.__path);
+      const srcPath = resolve(mdDir, props.src);
+      const code = fsExtra.readFileSync(srcPath, 'utf-8');
 
-    const demoScripts = getDemoComponent(md, {
-      title: props.title,
-      desc: props.desc,
-      path: srcPath,
-      code,
-    });
+      const demoScripts = getDemoComponent(md, {
+        title: props.title,
+        desc: props.desc,
+        path: srcPath,
+        code,
+      });
 
-    return demoScripts;
+      return demoScripts;
+    };
   };
+
+  addRenderRule('html_block');
+  addRenderRule('html_inline');
 }
